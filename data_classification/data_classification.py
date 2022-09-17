e893f5f6f65cfda0f6fd1abfcfe38f48b6441efb
techpril <priscila.teodorio2015@gmail.com>
2022-09-17 11:45:18 -0300

    Organizando

---
+++ data_classification/data_classification.py
@@ -0,0 +1,38 @@
+# Package imports
+import numpy as np
+import matplotlib.pyplot as plt
+from testCases_v2 import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+
+np.random.seed(1) 
+
+X, Y = load_planar_dataset()
+
+# Exercise: How many training examples do you have? In addition, what is the shape of the variables X and Y?
+
+### START CODE HERE ### (≈ 3 lines of code)
+shape_X = X.shape
+shape_Y = Y.shape
+m = (X.size)/shape_X[0]  # training set size
+### END CODE HERE ###
+
+print ('The shape of X is: ' + str(shape_X))
+print ('The shape of Y is: ' + str(shape_Y))
+print ('I have m = %d training examples!' % (m))
+
+# Train the logistic regression classifier
+clf = sklearn.linear_model.LogisticRegressionCV();
+clf.fit(X.T, Y.ravel());
+
+# Plot the decision boundary for logistic regression
+plot_decision_boundary(lambda x: clf.predict(x), X, Y)
+plt.title("Logistic Regression")
+
+# Print accuracy
+LR_predictions = clf.predict(X.T)
+print ('Accuracy of logistic regression: %d ' % float((np.dot(Y,LR_predictions) + np.dot(1-Y,1-LR_predictions))/float(Y.size)*100) +
+       '% ' + "(percentage of correctly labelled datapoints)")
